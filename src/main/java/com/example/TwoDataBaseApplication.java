cb42905ea102925c7ab16e2d305120e8ae042b6a
Gaurav-S26 <160576236+Gaurav-S26@users.noreply.github.com>
2024-03-06 10:22:36 +0530

    Update TwoDataBaseApplication.java

--- src/main/java/com/example/TwoDataBaseApplication.java
+++ src/main/java/com/example/TwoDataBaseApplication.java
@@ -14,10 +14,7 @@ public class TwoDataBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(TwoDataBaseApplication.class, args);
     }
-     @GetMapping("/test")
-    public String getOrdersone(){
-        return  "This is Example file are one";
-    }
+    
     @Bean
     ModelMapper getModelMapper() {
         return new ModelMapper();
